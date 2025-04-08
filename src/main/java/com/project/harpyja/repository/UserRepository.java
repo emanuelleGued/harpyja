7714d719b8d92803829b30e45fbb50a04982e6d2
emanuelleGued <manugguedes1701@gmail.com>
2025-04-07 23:25:42 -0300

    add findUserWithProjectKeyByEmail in UserRepository

--- src/main/java/com/project/harpyja/repository/UserRepository.java
+++ src/main/java/com/project/harpyja/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.project.harpyja.repository;
 
 import com.project.harpyja.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -9,4 +11,13 @@ import java.util.UUID;
 public interface UserRepository extends JpaRepository<User, UUID> {
 
     Optional<User> findByEmail(String email);
+    @Query("""
+    SELECT u.id AS userId, p.key AS projectKey
+    FROM User u
+    JOIN UserProject up ON u.id = up.user.id
+    JOIN Project p ON up.project.id = p.id
+    WHERE u.email = :email
+""")
+    UserWithProjectKey findUserWithProjectKeyByEmail(@Param("email") String email);
+
 }
