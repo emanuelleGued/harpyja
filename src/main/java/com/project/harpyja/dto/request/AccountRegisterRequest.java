876119343cb91f263ed1dc37c6e55ff95ee929e3
thisames <thiiagosantos300@gmail.com>
2025-04-15 16:37:56 -0300

    add dto request and response

---
+++ src/main/java/com/project/harpyja/dto/request/AccountRegisterRequest.java
@@ -0,0 +1,68 @@
+package com.project.harpyja.dto.request;
+
+public class AccountRegisterRequest {
+    private String userName;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getOrganizationName() {
+        return organizationName;
+    }
+
+    public void setOrganizationName(String organizationName) {
+        this.organizationName = organizationName;
+    }
+
+    public String getBusinessSize() {
+        return businessSize;
+    }
+
+    public void setBusinessSize(String businessSize) {
+        this.businessSize = businessSize;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    private String organizationName;
+    private String businessSize;
+    private String country;
+    private String city;
+    private String street;
+    private String zip;
+}
